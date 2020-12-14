6cb88f2774e6d7d364054c6c8f8fdd9cdb018f0f
Dartegnian <root@dartegnian.com>
2020-12-14 09:46:16 +0800

    Adjust spacing at the start of the system memory info

--- update_motd.sh
+++ update_motd.sh
@@ -4,7 +4,7 @@ print_memory_status () {
 	local total_memory=$(grep MemTotal /proc/meminfo | grep -o -P "\d{1,}")
 	local available_memory=$(grep MemAvailable /proc/meminfo | grep -o -P "\d{1,}")
 
-	echo "total real memory         = ${total_memory}"
+	printf "\ntotal real memory         = ${total_memory}\n"
 	echo "total available memory    = ${available_memory}"
 }
 print_os_info () {
