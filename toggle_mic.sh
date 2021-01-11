aece91776032faa49502ce55ce75ee75966d3918
Dartegnian <root@dartegnian.com>
2021-01-11 17:52:39 +0800

    Write script for querying current PulseAudio mic and toggling mute

---
+++ toggle_mic.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+query_pulseaudio () {
+	local default_source=$(pactl info | grep -P -o "alsa_input.*")
+	printf "${default_source}\n"
+	query_microphone_mute_status $default_source
+}
+query_microphone_mute_status () {
+	local mute_status=$(pactl list sources | grep -C 6 $1 | tail -n 1)
+	local is_muted=$(echo $mute_status | grep -P -o "[^Mute\: ].*")
+	printf "${is_muted}\n"
+	set_source_mute_status $is_muted $1
+}
+set_source_mute_status () {
+	if [ $1 == "yes" ]; then
+		$(pactl set-source-mute $2 false)
+	else 
+		$(pactl set-source-mute $2 true)
+	fi
+}
+
+query_pulseaudio
