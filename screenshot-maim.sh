2bea7d9d19679207fc39c77af8cbd39236812613
Dartegnian <root@dartegnian.com>
2021-12-28 14:02:02 +0800

    Write script for saving named maim full and partial screenshots

---
+++ screenshot-maim.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+timestamp=$(date +%Y-%m-%d--%H-%M-%S)
+
+take_screenshot() {
+	local kernel_name=$(uname -s | tr '[:upper:]' '[:lower:]')
+	local file_suffix=""
+	local params=""
+	local location=""
+	local os_name=$(cat /etc/*-release | grep PRETTY_NAME.* | tr '[:upper:]' '[:lower:]')
+	os_name=${os_name/pretty_name=/}
+	os_name=${os_name//\"/}
+	os_name=${os_name/ $kernel_name/}
+
+	if [[ $1 == "screen" ]]; then
+		params="-m 10 -f png"
+		file_suffix="-screen.png"
+		location="/home/$USER/Pictures/Saved Folders/Screenshots/Arch Linux"
+	elif [[ $1 == "area" ]]; then
+		params="-s -m 10 -f png"
+		file_suffix="-screencap.png"
+		location="/tmp"
+	fi
+
+	local file_name="${timestamp}-${os_name}-${kernel_name}-${file_suffix}"
+
+	maim $params "$location"/$file_name
+	sleep 0.25
+	xclip -selection clipboard -t image/png "$location"/$file_name
+}
+
+take_screenshot $1
+
