71087d92c4120f16257a565b0bf801e5129f4391
Dartegnian <root@dartegnian.com>
2022-04-22 07:22:56 +0800

    Write script for restoring capable dotfiles using GNU Stow

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+
+context=$(pwd)
+git_directory="/home/${USER}/SYGtech/goteki-git/darts-dots"
+device_category="pc"
+distro_name=$(cat /etc/*-release | grep DISTRIB_ID.* | grep -o -P "[^DISTRIB_ID=].*" | sed 's/"//g'| awk '{print tolower($0)}')
+computer_name="main-pc"
+backup_folder_location="${git_directory}/${device_category}/${distro_name}-${computer_name}"
+conf_folder_location="${backup_folder_location}/config"
+
+restore_gnu_stow() {
+	programs=(
+		alacritty
+		alsa
+		bspwm
+		ckb-next
+		miniplayer
+		mpDris2
+		nomacs
+		picom
+		sxhkd
+		wal
+	)
+
+	cd ${conf_folder_location}
+
+	for folder in ${programs[@]}; do
+		stow $folder -t ~/
+	done
+
+	cd "$context"
+}
+
+main() {
+	restore_gnu_stow
+}
+
+main
