b5c2759c1c01d1ddd3bbdfed242578948fe85894
Dartegnian <root@dartegnian.com>
2022-05-02 09:46:57 +0800

    Use sed-based, StackOverflow-based formatting option

--- mortality.sh
+++ mortality.sh
@@ -4,6 +4,6 @@ date_today=$(date +'%Y-%m-%d')
 death_date='2034-01-19'
 
 days_remaining=$(( ($(date -d $death_date +%s) - $(date -d $date_today +%s)) / 86400 ))
-formatted_days=$(printf "%'.0f" $days_remaining)
+formatted_days=$(echo $days_remaining | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
 
 echo $formatted_days 'days'
