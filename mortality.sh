b64e8d6df3df1c5e30c3b9fbd8e00e8d46859d06
Dartegnian <root@dartegnian.com>
2022-05-03 13:03:27 +0800

    Allow numerical value only if -d is passed

--- mortality.sh
+++ mortality.sh
@@ -6,4 +6,8 @@ death_date='2034-01-19'
 days_remaining=$(( ($(date -d $death_date +%s) - $(date -d $date_today +%s)) / 86400 ))
 formatted_days=$(echo $days_remaining | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')
 
-echo $formatted_days 'days'
+if [[ $1 == "-d" ]]; then
+	echo $formatted_days
+else
+	echo $formatted_days 'days'
+fi
