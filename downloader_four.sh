6b44fa870108e5cdfcd7c159593a0b9cec7d240a
Dartegnian <root@dartegnian.com>
2022-06-11 02:02:06 +0800

    Put commands into function, add usage/program help if no params

--- downloader_four.sh
+++ downloader_four.sh
@@ -1,8 +1,25 @@
 #! /bin/sh
 
-title_tag=$(curl $1 | grep -oP "(?<=<title>).*(?=<\/title>)")
-download_directory="/home/$USER/Downloads"
-directory_name=$(echo $title_tag | grep -oP -m 1 "(?<=- )([^-]+)(?= -)" | head -1)
-directory_name="${directory_name/\//-}"
+function print_usage() {
+	cat << EOF
+Usage:
+ downloader_four.sh [URL]
+EOF
+}
+function download_thread() {
+	local title_tag=$(curl $1 | grep -oP "(?<=<title>).*(?=<\/title>)")
+	local download_directory="/home/$USER/Downloads"
+	local directory_name=$(echo $title_tag | grep -oP -m 1 "(?<=- )([^-]+)(?= -)" | head -1)
+	local directory_name="${directory_name/\//-}"
 
-wget -P "$download_directory/$directory_name" -nd -r -l 1 -H -D i.4cdn.org,is2.4chan.org -A png,gif,jpg,jpeg,webm -R '?????????????s.*' $1
+	wget -P "$download_directory/$directory_name" -nd -r -l 1 -H -D i.4cdn.org,is2.4chan.org -A png,gif,jpg,jpeg,webm -R '?????????????s.*' $1
+}
+function main() {
+	if [[ $1 ]]; then
+		download_thread
+	else
+		print_usage
+	fi
+}
+
+main $1
