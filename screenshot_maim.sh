4feff6d89521e6bfeae68dc99372ce609507b171
Dartegnian <root@dartegnian.com>
2022-06-11 01:58:38 +0800

    Refactor, put commands into functions, add program help/usage func

--- screenshot_maim.sh
+++ screenshot_maim.sh
@@ -1,32 +1,74 @@
 #! /bin/sh
 
-timestamp=$(date +%Y-%m-%d--%H-%M-%S)
+function print_usage() {
+	cat << EOF
+Usage:
+ screenshot_main.sh [option]
 
-kernel_name=$(uname -s | tr '[:upper:]' '[:lower:]')
-file_suffix=""
-params=""
-location=""
-os_name=$(cat /etc/*-release | grep PRETTY_NAME.* | tr '[:upper:]' '[:lower:]')
-os_name=${os_name/pretty_name=/}
-os_name=${os_name//\"/}
-os_name=${os_name/ $kernel_name/}
+Options:
+ -s, --screen		Take a full screen screenshot and save to disk
+ -t, --screen-temp	Take a full screen screenshot and save to /tmp
+ -a, --area		Take a screenshot of an area on the screen
+ -h, --help		Print the program usage
 
-if [[ $1 == "screen" ]]; then
-	params="-m 10 -f png"
-	file_suffix="-screen.png"
-	location="/home/$USER/Pictures/Saved Folders/Screenshots/Arch Linux"
-elif [[ $1 == "screen-temp" ]]; then
-	params="-m 10 -f png"
-	file_suffix="-screenshot.png"
-	location="/tmp"
-elif [[ $1 == "area" ]]; then
-	params="-s -m 10 -f png"
-	file_suffix="-screencap.png"
-	location="/tmp"
-fi
+All options (except help) copy the screenshot to your clipboard.
+EOF
+}
+function set_global_variables() {
+	timestamp=$(date +%Y-%m-%d--%H-%M-%S)
 
-file_name="${timestamp}-${os_name}-${kernel_name}-${file_suffix}"
+	kernel_name=$(uname -s | tr '[:upper:]' '[:lower:]')
+	file_suffix=""
+	params=""
+	location=""
 
-maim $params "$location"/$file_name
-sleep 0.5
-xclip -selection clipboard -t image/png "$location"/$file_name
+	os_name=$(cat /etc/*-release | grep PRETTY_NAME.* | tr '[:upper:]' '[:lower:]')
+	os_name=${os_name/pretty_name=/}
+	os_name=${os_name//\"/}
+	os_name=${os_name/ $kernel_name/}
+}
+function decipher_params() {
+	case $1 in
+		-s|--screen)
+			params="-m 10 -f png"
+			file_suffix="-screen.png"
+			location="/home/$USER/Pictures/Saved Folders/Screenshots/Arch Linux"
+			take_screenshot
+			;;
+		-t|--screen-temp)
+			params="-m 10 -f png"
+			file_suffix="-screenshot.png"
+			take_screenshot
+			;;
+		-a|--area)
+			params="-s -m 10 -f png"
+			file_suffix="-screencap.png"
+			location="/tmp"
+			take_screenshot
+			;;
+		-h|--help)
+			print_usage
+			;;
+		*)
+			printf "Invalid option: $1\n\n"
+			print_usage
+			;;
+	esac
+}
+function take_screenshot() {
+	file_name="${timestamp}-${os_name}-${kernel_name}-${file_suffix}"
+
+	maim $params "$location"/$file_name
+	sleep 0.5
+	xclip -selection clipboard -t image/png "$location"/$file_name
+}
+function main() {
+	if [[ $1 ]]; then
+		set_global_variables
+		decipher_params $1
+	else
+		print_usage
+	fi
+}
+
+main $1
