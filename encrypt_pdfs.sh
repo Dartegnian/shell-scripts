c260906b9757b033395dc9a10321512b01b4334a
Dartegnian <root@dartegnian.com>
2024-04-02 18:30:46 +0800

    Change algorithm for password generation, only use A-Z, a-z, 0-9

--- encrypt_pdfs.sh
+++ encrypt_pdfs.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 for i in *.pdf; do
-	password=$(openssl rand -base64 21)
+	password=$(openssl rand -base64 21 | tr -dc 'a-zA-Z0-9' | head -c 21)
 	stapler -u "$password" cat "$i" "$i-encrypted.pdf"
 	echo "${i}: $password" >> pdf-passwords.txt
 done
